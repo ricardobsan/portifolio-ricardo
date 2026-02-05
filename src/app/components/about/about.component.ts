6f450f9b1469e370e20ef3a1d248d01b41e4842f
Ricardo <dev.ricardosantos@gmail.com>
2026-02-05 01:02:06 -0300

    Finalizado o About

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { IonGrid, IonRow, IonCol } from "@ionic/angular/standalone";
+import { IonGrid, IonRow, IonCol, IonButton, IonIcon } from "@ionic/angular/standalone";
+import { addIcons } from 'ionicons';
+import { downloadOutline, eyeOutline,  } from 'ionicons/icons';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
-  imports: [IonGrid, IonRow, IonCol],
+  imports: [IonIcon, IonButton, IonGrid, IonRow, IonCol],
 })
-export class AboutComponent  implements OnInit {
+export class AboutComponent implements OnInit {
+  aboutExpanded = false;
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      downloadOutline,
+      eyeOutline
+    })
 
-  ngOnInit() {}
+
+  }
+
+  ngOnInit() { }
 
 }
