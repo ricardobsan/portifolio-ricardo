c436df3930fcc97606021aedf8c93cc6c8b9bf65
Ricardo <dev.ricardosantos@gmail.com>
2026-02-11 23:47:03 -0300

    Desenvolvido os cards, grid desktop e swipper mobile.

---
+++ src/app/components/projects/projects.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { logoGithub, openOutline, } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] // Importante adicionar aqui!
+})
+export class ProjectsComponent implements OnInit {
+
+  activeIndex = 0;
+
+  constructor() {
+    addIcons({
+      openOutline,
+      logoGithub
+    })
+
+  }
+
+  ngOnInit() { }
+
+  projects = [
+    {
+      title: 'Developer Portfolio',
+
+      image: 'assets/images-projects/portifolio.png',
+      techs: ['Angular', 'Ionic', 'Firebase'],
+      description: 'Portfólio pessoal criado para apresentar projetos e habilidades como desenvolvedor front-end.',
+      githubUrl: 'https://github.com/ricardobsan/portifolio-ricardo.git',
+      liveUrl: 'https://devricardosantos.com.br'
+    },
+    {
+      title: 'OdontoCare Website',
+      description: 'Website institucional responsivo para clínica odontológica.',
+      techs: ['HTML', 'CSS', 'JavaScript'],
+      image: 'assets/images-projects/odonto.png',
+      githubUrl: 'https://github.com/ricardobsan/projetoOdonto.git',
+      liveUrl: 'https://ricardobsan.github.io/projetoOdonto/index.html'
+    },
+    {
+      title: 'Task List App',
+      description: 'Aplicação para gerenciamento de tarefas com status e subtarefas.',
+      techs: ['Angular', 'TypeScript', 'CSS'],
+      image: 'assets/images-projects/todoList.png',
+      githubUrl: 'https://github.com/ricardobsan/ListaDeTarefas.git',
+      liveUrl: 'https://ricardobsan.github.io/ListaDeTarefas/'
+    },
+    {
+      title: 'Calculator App',
+      description: 'Calculadora simples com operações básicas.',
+      techs: ['HTML', 'CSS', 'JavaScript'],
+      image: 'assets/images-projects/calc.png',
+      githubUrl: 'https://github.com/ricardobsan/ProjetoCalculadoraSimples.git',
+      liveUrl: 'https://ricardobsan.github.io/ProjetoCalculadoraSimples/'
+    },
+
+    {
+      title: 'Em breve',
+      image: 'assets/images-projects/em-breve.png',
+      description: 'Em Breve!'
+    },
+  ];
+
+  get activeProject() {
+    return this.projects[this.activeIndex];
+  }
+
+  onSlideChange(event: any) {
+    this.activeIndex = event.target.swiper.realIndex;
+  }
+
+}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -6,13 +6,14 @@ import { AboutComponent } from "src/app/components/about/about.component";
 import { FooterComponent } from "src/app/components/footer/footer.component";
 import { HeaderComponent } from "src/app/components/header/header.component";
 import { ToolbarComponent } from "src/app/components/toolbar/toolbar.component";
+import { ProjectsComponent } from "src/app/components/projects/projects.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, CommonModule, FormsModule, AboutComponent, FooterComponent, HeaderComponent, ToolbarComponent]
+  imports: [IonContent, IonHeader, CommonModule, FormsModule, AboutComponent, FooterComponent, HeaderComponent, ToolbarComponent, ProjectsComponent]
 })
 export class HomePage implements OnInit {
 
