b71e28b1ab7a5e74927667b7763115b8bc2d80a0
Ricardo <dev.ricardosantos@gmail.com>
2026-02-15 22:56:50 -0300

    Função dos botões

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonMenuToggle } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem],
+  imports: [IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonMenuToggle],
 })
 export class AppComponent {
 
@@ -13,13 +13,11 @@ export class AppComponent {
 constructor(private router: Router, private menuCtrl: MenuController) {}
 
 async navigate(section: string) {
-  await this.menuCtrl.close();
+  // Agora o ID 'mainMenu' existe no HTML
+  await this.menuCtrl.close('mainMenu'); 
 
-  setTimeout(() => {
-    this.router.navigate(['/home'], {
-      fragment: section
-    });
-  }, 250);
+  this.router.navigate(['/home'], {
+    fragment: section
+  });
 }
-
 }
